3a03cfcd48220adbeb67880d47b5f33c80652e00
J. Scott Smith <scott@newleafsolutionsinc.com>
2016-03-05 01:37:30 -0800

    Fixed nav header, added more content and team section

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,56 +1,12 @@
 
-// $(function() {
+$(function() {
+  "use strict";
 
-//   "use strict";
-
-//   var step = 0;
-
-//   setInterval(function() {
-//     $('.hf-blue').toggleClass('hf-red');
-
-//   // setInterval(function() {
-//   //   step++;
-//   //   switch (step) {
-//   //     case 1:
-//   //     case 2:
-//   //       $('.hf-ethos-impact').toggleClass('hf-highlight');
-//   //       break;
-//   //     case 3:
-//   //     case 4:
-//   //       $('.hf-ethos-endure').toggleClass('hf-highlight');
-//   //       break;
-//   //     case 5:
-//   //       $('.hf-ethos-connect').toggleClass('hf-highlight');
-//   //       break;
-//   //     default:
-//   //       $('.hf-ethos-connect').toggleClass('hf-highlight');
-//   //       step = 0;
-//   //   }
-//   }, 2000);
-
-//   // var transitionEnd = 'webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend';
-
-//   // var animateStep1 = function() {
-//   //   setTimeout(function() {
-//   //     $('.hf-ethos-connect').removeClass('hf-highlight');
-//   //     $('.hf-ethos-impact').addClass('hf-highlight').one(transitionEnd, animateStep2);
-//   //   }, 2000);
-//   // };
-
-//   // var animateStep2 = function() {
-//   //   setTimeout(function() {
-//   //     $('.hf-ethos-impact').removeClass('hf-highlight');
-//   //     $('.hf-ethos-endure').addClass('hf-highlight').one(transitionEnd, animateStep3);
-//   //   }, 2000);
-//   // };
-
-//   // var animateStep3 = function() {
-//   //   setTimeout(function() {
-//   //     $('.hf-ethos-endure').removeClass('hf-highlight');
-//   //     $('.hf-ethos-connect').addClass('hf-highlight').one(transitionEnd, animateStep1);
-//   //   }, 2000);
-//   // };
-
-//   // animateStep1();
-
-// });
+  $(window).scroll(function() {
+    if ($(document).scrollTop() > 100) {
+      $('#homeNavbar.navbar').addClass('hf-scrolled');
+    } else {
+      $('#homeNavbar.navbar').removeClass('hf-scrolled');
+    }
+  });
+});
